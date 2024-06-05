48d066d9fc1b1a2e4d6cfe0cb15e1c8f752c8d66
Shubranshu Shekhar <shubranshumishra22@gmail.com>
2024-06-05 21:35:26 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,9 @@
+import conf from '../conf/conf';
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {}
+
+  //object for the user
+const authService = new AuthService();
+
+export default authService;
